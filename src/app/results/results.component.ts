4390ddcd089c10deb787152c40318ec22bc2ee45
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-08-12 17:32:28 -0400

    added welcome component and material

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Answers } from '../quiz.model';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.scss']
+})
+export class ResultsComponent {
+  @Input() answers: Answers;
+}
